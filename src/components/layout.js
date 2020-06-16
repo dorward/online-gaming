0f3d5963ab4b5000a2f27401807fe29fd29b23fc
David Dorward <david@dorward.me.uk>
2020-06-16 15:58:53 +0100

    Make mobile friendly

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,31 +5,46 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
 
 import Header from "./header";
 import Menu from "./menu";
 
+import "../../node_modules/hamburgers/dist/hamburgers.min.css";
 import "./layout.css";
 
 const Layout = ({ children }) => {
-	const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
+	const data = useStaticQuery(
+		graphql`
+    		query SiteTitleQuery {
+      			site {
+        			siteMetadata {
+          			title
+				}
+      		}
+		}`
+	);
+
+	const [menuOpen, setMenuOpen] = useState(false);
+	const menuClasses = ["hamburger", "hamburger--collapse"];
+	if (menuOpen) {
+		menuClasses.push("is-active");
+	}
 
 	return (
 		<div className="wrapper">
 			<Header siteTitle={data.site.siteMetadata.title} />
 			<div className="container">
-				<nav><Menu /></nav>
+				<nav>
+					<button className={menuClasses.join(" ")} type="button" onClick={() => setMenuOpen(!menuOpen)} aria-expanded={menuOpen} aria-label="Toggle menu">
+						<span className="hamburger-box">
+							<span className="hamburger-inner"></span>
+						</span>
+					</button>
+					<Menu />
+				</nav>
 				<main>{children}</main>
 			</div>
 			<footer>
